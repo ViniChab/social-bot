ff101962ca9901b534033823ba0182ee8e2ae1d3
Vinícius S. Chab <chabvini@gmail.com>
2022-01-27 23:17:32 -0300

    Loading default chart

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const PuppeteerService = require("./services/puppeteer/pupeteer.service");
 const BrowserHandler = require("./services/browser-handler/browser-handler.service");
 const LinkedinCoordinatorService = require("./services/linkedin-coordinator/linkedin-coordinator.service");
 
-const port = process?.env?.PORT || 5100;
 const Express = require("express");
+const Path = require("path");
+
+const port = process?.env?.PORT || 5100;
 const app = Express();
+
+app.get("/", (req, res) => {
+  res.sendFile(Path.join(__dirname, "./pages", "main.html"));
+});
 
 app.listen(port, () => {
   console.log(`### API RESTARTED ON PORT: ${port}`);
-  main();
+  // main();
 });
 
 // Main
